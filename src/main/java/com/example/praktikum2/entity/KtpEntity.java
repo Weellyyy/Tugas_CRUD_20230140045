2c4b41871436903d38a3caa2333de8e349e266e5
Weellyyy <wildanprtm2304@gmail.com>
2026-03-11 20:19:50 +0700

    membuat entity untuk data ktp

---
+++ src/main/java/com/example/praktikum2/entity/KtpEntity.java
@@ -0,0 +1,33 @@
+package com.example.praktikum2.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "ktp")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KtpEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nomor_ktp", unique = true)
+    private String nomorKtp;
+
+    @Column(name = "nama_lengkap")
+    private String namaLengkap;
+
+    private String alamat;
+
+    @Column(name = "tanggal_lahir")
+    private Date tanggalLahir;
+
+    @Column(name = "jenis_kelamin")
+    private String jenisKelamin;
+}
