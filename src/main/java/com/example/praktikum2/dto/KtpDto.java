8057acb13fe358d2d44ef36b57eea409e31de273
Weellyyy <wildanprtm2304@gmail.com>
2026-03-11 20:20:27 +0700

    membuat dto untuk data ktp

---
+++ src/main/java/com/example/praktikum2/dto/KtpDto.java
@@ -0,0 +1,16 @@
+package com.example.praktikum2.dto;
+
+import lombok.Data;
+import java.util.Date;
+
+@Data
+public class KtpDto {
+
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private Date tanggalLahir;
+    private String jenisKelamin;
+
+}
