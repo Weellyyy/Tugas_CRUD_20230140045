e0a27c7f57be18077a999da2ac4dbab7dac6e647
Weellyyy <wildanprtm2304@gmail.com>
2026-03-11 20:35:23 +0700

    menyesuaikan repo

--- src/main/java/com/example/praktikum2/repository/KtpRepository.java
+++ src/main/java/com/example/praktikum2/repository/KtpRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface KtpRepository extends JpaRepository<KtpEntity,Integer> {
+public interface KtpRepository extends JpaRepository<KtpEntity, Integer> {
 
     Optional<KtpEntity> findByNomorKtp(String nomorKtp);
 
