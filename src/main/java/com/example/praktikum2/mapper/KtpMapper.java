fcc4d4b7497f368972315013284f15313e305650
Weellyyy <wildanprtm2304@gmail.com>
2026-03-11 20:34:00 +0700

    membuat mapper

---
+++ src/main/java/com/example/praktikum2/mapper/KtpMapper.java
@@ -0,0 +1,15 @@
+package com.example.praktikum2.mapper;
+
+import com.example.praktikum2.model.dto.KtpDto;
+import com.example.praktikum2.model.entity.KtpEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface KtpMapper {
+
+    KtpMapper MAPPER = Mappers.getMapper(KtpMapper.class);
+
+    KtpDto toKtpDtoData(KtpEntity ktpEntity);
+
+}
