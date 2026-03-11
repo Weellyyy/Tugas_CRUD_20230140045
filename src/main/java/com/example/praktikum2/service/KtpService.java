25db5872eeb2a629f5cd74e531a008fc933c0a3d
Weellyyy <wildanprtm2304@gmail.com>
2026-03-11 20:35:31 +0700

    membuat service

---
+++ src/main/java/com/example/praktikum2/service/KtpService.java
@@ -0,0 +1,18 @@
+package com.example.praktikum2.service;
+
+import com.example.praktikum2.model.dto.KtpDto;
+import java.util.List;
+
+public interface KtpService {
+
+    KtpDto create(KtpDto dto);
+
+    List<KtpDto> findAll();
+
+    KtpDto findById(Integer id);
+
+    KtpDto update(Integer id, KtpDto dto);
+
+    void delete(Integer id);
+
+}
